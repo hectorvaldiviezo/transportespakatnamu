ffaa6a5876e3fa80e8325be65dbc3389be8aab73
david3010M <hvaldiviezos@unprg.edu.pe>
2025-03-03 09:56:22 -0500

    feat: update config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const MILLA_BASE = "https://milla.grupopakatnamu.com/api";
-// const MILLA_BASE = "http://127.0.0.1:8000/api";
+const MILLA_BASE = "https://milla.grupopakatnamu.com";
+// const MILLA_BASE = "http://127.0.0.1:8000";
 const MILLA_URL = "/libro-reclamaciones";
+const API_URL = "/api";
 
-const api = axios.create({ baseURL: MILLA_BASE + MILLA_URL });
-const apiMilla = axios.create({ baseURL: MILLA_BASE });
+const api = axios.create({ baseURL: MILLA_BASE + API_URL + MILLA_URL });
+const apiMilla = axios.create({ baseURL: MILLA_BASE + API_URL });
 
 export { MILLA_BASE, MILLA_URL, api, apiMilla };
