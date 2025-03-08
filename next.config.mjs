0ccf196f548bedddc28687b58b7a5cf868574179
david3010M <hvaldiviezos@unprg.edu.pe>
2025-03-08 12:51:53 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/transportespakatnamu',
+    basePath: "/transportespakatnamu",
+    assetPrefix: "/transportespakatnamu",
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
