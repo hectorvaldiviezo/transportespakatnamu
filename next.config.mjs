0bebbcc87df7953c2650bac3f15e3a888475fe95
david3010M <hvaldiviezos@unprg.edu.pe>
2025-03-10 17:23:10 -0500

    feat:cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/transportespakatnamu",
-    assetPrefix: "/transportespakatnamu/",
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
     images: {
-        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'http',
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ const MILLA_BASE = "https://milla.grupopakatnamu.com";
 // const MILLA_BASE = "http://127.0.0.1:8000";
 const MILLA_URL = "/libro-reclamaciones";
 const API_URL = "/api";
-const BASE_PATH = "/transportespakatnamu"
+const BASE_PATH = ""
 
 const api = axios.create({ baseURL: MILLA_BASE + API_URL + MILLA_URL });
 const apiMilla = axios.create({ baseURL: MILLA_BASE + API_URL });
