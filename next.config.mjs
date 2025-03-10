7dab74572d31449a1908d7000af49e2df890da6a
hectorvaldiviezo <hvaldiviezos@transportespakatnamu.com>
2025-03-10 09:53:59 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/transportespakatnamu",
-    assetPrefix: "/transportespakatnamu",
+    assetPrefix: "/transportespakatnamu/",
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
